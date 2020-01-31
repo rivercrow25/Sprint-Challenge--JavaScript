df21b708fc509714e3e20501d821b58a56a25308
nick migel <nickmigel2@gmail.com>
2020-01-31 15:35:21 -0500

    did it by being heavy handed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -90,13 +90,9 @@ const contactInfo = graduates.map(function (item) {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = []
-for (let i = 0; i < universities.length; i++) {
-  if (universities.includes('University') !== -1) {
-    unisWithUni.push(graduates[i])
-  }
-}
-
+let unisWithUni = graduates.filter(function (item) {
+  return item.university.includes('Uni')
+})
 
 console.log(unisWithUni);
 
